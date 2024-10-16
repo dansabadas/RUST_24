9bde72fe8749297d448e3b121744c489424411e5
Dan Sabadis <dsabadas@strategikonpharma.com>
2024-10-16 11:43:30 +0300

    arroved at ch6 pg 110 and hashmaps

---
+++ ch06/src/main.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+use std::collections::BTreeMap;
+
+struct City {
+    name: String,
+    population: HashMap<i32, i32>,
+}
+
+struct City2 {
+    name: String,
+    population: BTreeMap<i32, i32>,
+}
+
+fn main() {
+    let mut tallinn = City {
+        name: "Tallinn".to_string(),
+        population: HashMap::new(),
+    };
+
+    tallinn.population.insert(2020, 437_619);
+    tallinn.population.insert(1372, 3_250);
+    tallinn.population.insert(1851, 24_000);
+
+    for (year, population) in tallinn.population {
+        println!("In {year}, Tallinn had a population of {population}.");
+    }
+
+    let mut tallinn2 = City2 {
+        name: "Tallinn".to_string(),
+        population: BTreeMap::new(),
+    };
+
+    tallinn2.population.insert(2020, 437_619);
+    tallinn2.population.insert(1372, 3_250);
+    tallinn2.population.insert(1851, 24_000);
+    for (year, population) in tallinn2.population {
+        println!("In {year}, Tallinn had a population of {population}.");
+    }
+
+    let canadian_cities = vec!["Calgary", "Vancouver", "Gimli"];
+    let german_cities = vec!["Karlsruhe", "Bad Doberan", "Bielefeld"];
+
+    let mut city_hashmap = HashMap::new();
+    for city in canadian_cities {
+        city_hashmap.insert(city, "Canada");
+    }
+    for city in german_cities {
+        city_hashmap.insert(city, "Germany");
+    }
+    println!("{:?}", city_hashmap["Bielefeld"]);
+    println!("{:?}", city_hashmap.get("Bielefeld"));
+    println!("{:?}", city_hashmap.get("Bielefeldd"));
+    println!("{:?}", city_hashmap.get("Bielefeld"));
+
+    let mut book_hashmap = HashMap::new();
+    book_hashmap.insert(1, "L'Allemagne Moderne");
+    book_hashmap.insert(1, "Le Petit Prince");
+    book_hashmap.insert(1, "섀도우 오브 유어 스마일");
+    book_hashmap.insert(1, "Eye of the World");
+    println!("{:?}", book_hashmap.get(&1));
+}
