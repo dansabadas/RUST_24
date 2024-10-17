4fbb0fd60465ac2d3af52cff4d83485223ca47a7
Dan Sabadis <dsabadas@strategikonpharma.com>
2024-10-17 11:10:15 +0300

    arrived at 6.3 pg 122

--- ch06/src/main.rs
+++ ch06/src/main.rs
@@ -1,5 +1,10 @@
 use std::collections::HashMap;
 use std::collections::BTreeMap;
+use std::collections::HashSet;
+use std::collections::BTreeSet;
+use std::collections::BinaryHeap;
+use std::collections::VecDeque;
+use std::num::ParseIntError;
 
 struct City {
     name: String,
@@ -130,4 +135,113 @@ fn main() {
     for (male_or_female, numbers) in survey_hash {
         println!("{male_or_female}: {numbers:?}");
     }
+    let many_numbers = vec![
+        37, 3, 25, 11, 27, 3, 37, 21, 36, 19, 37, 30, 48, 28, 16, 33, 2, 10, 1, 12, 38, 35, 30, 21,
+        20, 38, 16, 48, 39, 31, 41, 32, 50, 7, 15, 1, 20, 3, 33, 12, 1, 11, 34, 38, 49, 1, 27, 9,
+        46, 33,
+    ];
+    println!("How many numbers in the Vec? {}", many_numbers.len());
+
+    let mut number_hashset = HashSet::new();
+    for number in many_numbers {
+        number_hashset.insert(number);
+    }
+    let hashset_length = number_hashset.len();
+    println!(
+        "There are {hashset_length} unique numbers, so we are missing {}.",
+        50 - hashset_length
+    );
+    println!("It does not contain: ");
+    for number in 0..=50 {
+        if number_hashset.get(&number).is_none() {
+            print!("{number} ");
+        }
+    }
+    for entry in number_hashset {
+        print!("{} ", entry);
+    }
+
+    let many_numbers = vec![37, 3, 25, 11, 27, 3, 37, 21, 36, 19, 37, 30, 48,
+        28, 16, 33, 2, 10, 1, 12, 38, 35, 30, 21, 20, 38, 16, 48, 39, 31, 41,
+        32, 50, 7, 15, 1, 20, 3, 33, 12, 1, 11, 34, 38, 49, 1, 27, 9, 46, 33];
+    let mut current_number = i32::MIN;
+    let mut number_set = BTreeSet::new();
+    for number in many_numbers {
+        number_set.insert(number);
+    }
+    for number in number_set {
+        if number < current_number {
+            println!("This will never happen");
+        }
+        current_number = number;
+    }
+    // for entry in number_set {
+    //     print!("{} ", entry);
+    // }
+
+    let many_numbers = vec![0, 5, 10, 15, 20, 25, 30];
+    let mut heap = BinaryHeap::new();
+    for num in many_numbers {
+        heap.push(num);
+    }
+    println!("First item is largest, others are out of order: {heap:?}");
+    while let Some(num) = heap.pop() {
+        println!("Popped off {num}. Remaining numbers are: {heap:?}");
+    }
+
+    let mut jobs = BinaryHeap::new();
+    jobs.push((100, "Reply to email from the CEO"));
+    jobs.push((80, "Finish the report today"));
+    jobs.push((5, "Watch some YouTube"));
+    jobs.push((70, "Tell your team members thanks for always working hard"));
+    jobs.push((30, "Plan who to hire next for the team"));
+    for (_, job) in jobs {
+        println!("You need to: {job}");
+    }
+    // while let Some(num) = jobs.pop() {
+    //     println!("Popped off. Remaining numbers are: {heap:?}");
+    // }
+
+    let mut my_vec = VecDeque::from(vec![0; 600000]);
+    for _ in 0..600000 {
+        my_vec.pop_front();
+        //my_vec.remove(0);
+    }
+
+    let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
+    for item in str_vec {
+        let parsed = parse_and_log_str(item);
+        println!("Result: {parsed:?}");
+    }
+    let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
+    for item in str_vec {
+        let parsed = parse_str(item);
+        println!("{parsed:?}");
+    }
+
+    let num = turn_into_string_and_parse(vec![49, 53, 53]);
+    println!("{num}");
+}
+
+fn turn_into_string_and_parse(bytes: Vec<u8>) -> i32 {
+    let as_string = String::from_utf8(bytes).unwrap();
+    let as_num = as_string.parse::<i32>().unwrap();
+    as_num
+}
+
+fn parse_str(input: &str) -> Result<i32, ParseIntError> {
+    let parsed_number = input
+        .parse::<u16>()?
+        .to_string()
+        .parse::<u32>()?
+        .to_string()
+        .parse::<i32>()?;
+    println!("Number parsed successfully into {parsed_number}");
+    Ok(parsed_number)
+}
+
+fn parse_and_log_str(input: &str) -> Result<i32, ParseIntError> {
+    let parsed_number = input.parse::<i32>()?;
+    println!("Number parsed successfully into {parsed_number}");
+    Ok(parsed_number)
 }
