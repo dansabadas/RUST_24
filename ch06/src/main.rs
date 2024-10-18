cac8cd65e5a16ea9e8baadd04e6683b51748a27f
Dan Sabadis <dsabadas@strategikonpharma.com>
2024-10-18 14:42:34 +0300

    arrived at 7

--- ch06/src/main.rs
+++ ch06/src/main.rs
@@ -221,6 +221,47 @@ fn main() {
 
     let num = turn_into_string_and_parse(vec![49, 53, 53]);
     println!("{num}");
+
+    let my_vec = vec![8, 9, 10];
+    print_all_three_things(my_vec);
+
+    let my_name = "Loki Laufeyson";
+    assert!(my_name == "Loki Laufeyson");
+    assert_eq!(my_name, "Loki Laufeyson");
+    assert_ne!(my_name, "Mithridates");
+
+    assert!(
+        my_name == "Loki Laufeyson",
+        "Name {my_name} is wrong: should be Loki Laufeyson"
+    );
+    assert_eq!(
+        my_name, "Loki Laufeyson",
+        "{my_name} and Loki Laufeyson should be equal"
+    );
+    assert_ne!(
+        my_name, "Mithridates",
+        "You entered {my_name}. Input must not equal Mithridates"
+    );
+
+    let my_vec = vec![9, 0, 10];
+    //let fourth = get_fourth(&my_vec);
+
+    let my_vec = vec![8, 9, 10];
+    let fourth = my_vec.get(3).unwrap_or(&0);
+    println!("{fourth}");
+}
+
+fn get_fourth(input: &Vec<i32>) -> i32 {
+    //let fourth = input.get(3).unwrap();
+    let fourth = input.get(3).expect("Input vector needs at least 4 items");
+    *fourth
+}
+
+fn print_all_three_things(vector: Vec<i32>) {
+    if vector.len() != 3 {
+        panic!("my_vec must always have three items");
+    }
+    println!("{}, {}, {}", vector[0], vector[1], vector[2]);
 }
 
 fn turn_into_string_and_parse(bytes: Vec<u8>) -> i32 {
