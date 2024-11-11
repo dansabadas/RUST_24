aa104d0f0ad936abe7540d650c9c56184455dcf1
DAN-STRATEGIKON\dan_s <dsabadas@strategikonpharma.com>
2024-11-11 10:30:07 +0200

    arrived at 346 on time.instant

--- ch17/src/main.rs
+++ ch17/src/main.rs
@@ -1,3 +1,79 @@
+use serde::{Serialize, Deserialize};
+use serde_json;
+
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let good_json_request = r#"
+    {
+        "name": "BillyTheUser",
+        "id": 6876
+    }
+    "#;
+    let bad_json_request = r#"
+    {
+        "name": "BobbyTheUser",
+        "idd": "6877"
+    }
+    "#;
+    handle_request(good_json_request);
+    handle_request(bad_json_request);
+
+    let time = Instant::now();
+    println!("{:?}", time);
+    println!("{:?}", Instant::now());
+
+    let start_of_main = Instant::now();
+    let before_operation = Instant::now();
+
+    let mut new_string = String::new();
+    loop {
+        new_string.push('წ');
+        if new_string.len() > 100_000 {
+            break;
+        }
+    }
+    
+    let after_operation = Instant::now();
+    println!("{:?}", before_operation - start_of_main);
+    println!("{:?}", after_operation - start_of_main);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct NewUserRequest {
+    name: String,
+    id: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    id: u32,
+    is_deleted: bool,
+}
+
+impl From<NewUserRequest> for User {
+    fn from(request: NewUserRequest) -> Self {
+        Self {
+            name: request.name,
+            id: request.id,
+            is_deleted: false,
+        }
+    }
+}
+
+fn handle_request(json_request: &str) {
+    match serde_json::from_str::<NewUserRequest>(json_request) {
+        Ok(good_request) => {
+            let new_user = User::from(good_request);
+            println!("Made a new user! {new_user:#?}");
+            println!(
+                "Serialized back into JSON: {:#?}",
+                serde_json::to_string(&new_user)
+            );
+        }
+        Err(e) => {
+            println!("Got an error from {json_request}: {e}");
+        }
+    }
 }
