d01fe079019751d9af4c06268219b06bafb34804
DAN-STRATEGIKON\dan_s <dsabadas@strategikonpharma.com>
2024-11-12 06:58:52 +0200

    arrived at 17.4 Rayon

--- ch17/src/main.rs
+++ ch17/src/main.rs
@@ -1,8 +1,12 @@
 use serde::{Serialize, Deserialize};
 use serde_json;
 
+use std::str::FromStr;
 use std::time::{Instant, SystemTime, UNIX_EPOCH, Duration};
 use std::thread::sleep;
+
+use chrono::naive::{NaiveDate, NaiveTime, NaiveDateTime};
+use chrono::{DateTime, FixedOffset, Utc};
 
 fn main() {
     let good_json_request = r#"
@@ -70,6 +74,92 @@ fn main() {
     println!("I must sleep now.");
     sleep(three_seconds);
     println!("Did I miss anything?");
+
+    println!("{:?}", NaiveDate::from_ymd_opt(2023, 3, 25));
+    println!("{:?}", NaiveTime::from_hms_opt(12, 5, 30));
+    println!("{:?}", NaiveDate::from_ymd_opt(2023, 3, 25).unwrap().and_hms_opt(12, 5, 30));
+
+    for _ in 0..1000 {
+        std::thread::spawn(|| {});
+    }
+
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+        let seconds = now.as_secs();
+        println!("Seconds from 1970 to today: {seconds}");
+        let naive_dt 
+            = NaiveDateTime::from_timestamp_opt(seconds as i64, 0).unwrap();
+        println!("As NaiveDateTime: {naive_dt}");
+        let utc_dt = DateTime::<Utc>::from_utc(naive_dt, Utc);
+
+        println!("As DateTime<Utc>: {utc_dt}");
+        let kyiv_offset = FixedOffset::east_opt(3 * 60 * 60).unwrap();
+        let kyiv_dt: DateTime::<FixedOffset> = DateTime::from_utc(naive_dt, kyiv_offset);
+        println!("In a timezone 3 hours from UTC: {kyiv_dt}");
+        let kyiv_naive_dt = kyiv_dt.naive_local();
+        println!("With timezone information removed: {kyiv_naive_dt}");
+
+        let incoming_event = UtcUserEvent {
+            timestamp: "2023-03-27 23:48:50 UTC",
+            data: "Something happened in UTC time".to_string(),
+        };
+        println!("Event as Utc:\n{incoming_event:?}");
+        let korea_japan_event = KoreaJapanUserEvent::from(incoming_event);
+        println!("Event in Korea/Japan time:\n{korea_japan_event:?}");
+}
+/////////
+
+const SECONDS_IN_HOUR: i32 = 3600;
+const UTC_TO_KST_HOURS: i32 = 9;
+const UTC_TO_KST_SECONDS: i32 = UTC_TO_KST_HOURS * SECONDS_IN_HOUR;
+
+#[derive(Debug)]
+struct UtcUserEvent {
+    timestamp: &'static str,
+    data: String,
+}
+
+#[derive(Debug)]
+struct KoreaJapanUserEvent {
+    timestamp: DateTime<FixedOffset>,
+    data: String,
+}
+
+impl From<UtcUserEvent> for KoreaJapanUserEvent {
+    fn from(event: UtcUserEvent) -> Self {
+        let utc_datetime: DateTime<Utc> = DateTime::from_str(event.timestamp).unwrap();
+        let offset = FixedOffset::east_opt(UTC_TO_KST_SECONDS).unwrap();
+        let timestamp: DateTime<FixedOffset> = DateTime::from_utc(utc_datetime.naive_utc(), offset);
+        Self {
+            timestamp,
+            data: event.data,
+        }
+    }
+}
+
+#[test]
+fn utc_to_korea_output_same_evening() {
+    let morning_event = UtcUserEvent {
+        timestamp: "2023-03-27 09:48:50 UTC",
+        data: String::new(),
+    };
+    let to_korea_japan = KoreaJapanUserEvent::from(morning_event);
+    assert_eq!(
+        &to_korea_japan.timestamp.to_string(),
+        "2023-03-27 18:48:50 +09:00"
+    );
+}
+
+#[test]
+fn utc_to_korea_output_next_morning() {
+    let evening_event = UtcUserEvent {
+        timestamp: "2023-03-27 23:59:59 UTC",
+        data: String::new(),
+    };
+    let korea_japan_next_morning = KoreaJapanUserEvent::from(evening_event);
+    assert_eq!(
+        &korea_japan_next_morning.timestamp.to_string(),
+        "2023-03-28 08:59:59 +09:00"
+    );
 }
 
 fn bad_random_number(digits: usize) {
