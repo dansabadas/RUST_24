3934f417ef56a65f18d93052743ab166909259c2
Dan Sabadis <dsabadas@strategikonpharma.com>
2024-11-13 09:58:26 +0200

    arrived at pg 383

--- ch18/src/main.rs
+++ ch18/src/main.rs
@@ -1,3 +1,40 @@
+use rand::seq::SliceRandom;
+use std::fmt::Display;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let my_letters = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+    let mut rng = rand::thread_rng();
+    for _ in 0..6 {
+        print!("{} ", my_letters.choose(&mut rng).unwrap());
+    }
+
+    let my_name = print_and_return("Windy");
+    let small_number = print_and_return(9.0);
+
+    println!("Please type something, or x to escape:");
+    let mut input_string = String::new();
+
+    while input_string.trim() != "x" {
+        input_string.clear();
+        io::stdin().read_line(&mut input_string).unwrap();
+        println!("You wrote {input_string}");
+    }
+    println!("See you later!");
+
+    println!("{:?}", std::env::args());
+}
+
+pub fn add() -> i32 {
+    let mut sum = 0;
+    for _ in 0..1000 {
+        sum += 1
+    }
+
+    sum
+}
+
+fn print_and_return<T: Display>(input: T) -> T {
+    println!("You gave me {input} and now I will give it back.");
+    input
 }
