eabbc5a84521c6bd4e931c7f92af05c54ce81009
Dan Sabadis <dsabadas@strategikonpharma.com>
2024-11-19 17:59:11 +0200

    arrived at 438

--- ch21/src/main.rs
+++ ch21/src/main.rs
@@ -1,3 +1,11 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Size of an i32: {}", mem::size_of::<i32>());
+    let my_array = [8; 50];
+    println!("Size of this array: {}", mem::size_of_val(&my_array));
+    let some_string = String::from("Droppable because it's not Copy");
+    println!("Size of this str: {}", mem::size_of_val(&some_string));
+    drop(some_string);
+    // some_string.clear();
 }
