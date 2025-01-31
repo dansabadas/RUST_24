d4eb4e683d3db84dd7394fd2fb967e3b15adde87
STRATEGIKON-DAN\dan_s <dsabadas@strategikonpharma.com>
2025-01-31 09:46:44 +0200

    arrived 8.2 coroutines

---
+++ idiomatic/ch08/src/main.rs
@@ -0,0 +1,109 @@
+use std::{collections::HashMap, marker::PhantomData};
+
+use uuid::Uuid;
+
+fn main() {
+    let session = Session::new();
+    println!("{:?}", session);
+    if let Ok(mut session) = session.authenticate("username", "password")
+    {
+        session.update_property("key", "value");
+        println!("{:?}", session);
+        let session = session.logout();
+        println!("{:?}", session);
+    }
+}
+
+pub trait SessionState {}
+
+#[derive(Debug, Default)]
+pub struct Session<State: SessionState = Initial> {
+    session_id: Uuid,
+    props: HashMap<String, String>,
+    phantom: PhantomData<State>,
+}
+
+#[derive(Debug, Default)]
+pub struct Initial;
+
+#[derive(Debug, Default)]
+pub struct Anonymous;
+
+#[derive(Debug, Default)]
+pub struct Authenticated;
+
+#[derive(Debug, Default)]
+pub struct LoggedOut;
+
+impl SessionState for Initial {}
+impl SessionState for Anonymous {}
+impl SessionState for Authenticated {}
+impl SessionState for LoggedOut {}
+
+#[derive(Debug)]
+pub enum ResumeResult {
+    Invalid,
+    Anonymous(Session<Anonymous>),
+    Authenticated(Session<Authenticated>),
+}
+
+impl Session<Initial> {
+    /// Returns a new session, defaulting to the anonymous state
+    pub fn new() -> Session<Anonymous> {
+        Session::<Anonymous> {
+            session_id: Uuid::new_v4(),
+            props: HashMap::new(),
+            phantom: PhantomData,
+        }
+    }
+
+    /// Returns the result of resuming this session from an existing ID.
+    pub fn resume_from(session_id: Uuid)
+        -> ResumeResult {
+            ResumeResult::Authenticated(
+                Session::<Authenticated> {
+                    session_id,
+                    props: HashMap::new(),
+                    phantom: PhantomData,
+                })
+    }
+}
+
+impl Session<Anonymous> {
+    pub fn authenticate(
+        self,
+        username: &str,
+        password: &str,
+    ) -> Result<Session<Authenticated>, Session<Anonymous>> {
+        // ...
+        if !username.is_empty() && !password.is_empty() {
+            Ok(Session::<Authenticated> {
+                session_id: self.session_id,
+                props: HashMap::new(),
+                phantom: PhantomData,
+            })
+        } else {
+            Err(self)
+        }
+    }
+}
+
+impl Session<Authenticated> {
+    pub fn update_property(&mut self, key: &str, value: &str) {
+        if let Some(prop) = self.props.get_mut(key) {
+            *prop = value.to_string();
+        } else {
+            self.props.insert(key.to_string(), value.to_string());
+        }
+        // ...
+    }
+    
+    pub fn logout(self) -> Session<LoggedOut> {
+            // ...
+        Session {
+            session_id: Uuid::nil(),
+            props: HashMap::new(),
+            phantom: PhantomData,
+        }
+    }
+}        
