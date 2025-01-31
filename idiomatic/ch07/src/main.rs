a1774ea77cf0dcca4c8e90c739ffd2dc0d3ecad4
STRATEGIKON-DAN\dan_s <dsabadas@strategikonpharma.com>
2025-01-31 09:14:35 +0200

    arrived at ch8

--- idiomatic/ch07/src/main.rs
+++ idiomatic/ch07/src/main.rs
@@ -19,7 +19,112 @@ fn main() {
 
     let other_reversed = reversed.iter().to_reversed();
     dbg!(&other_reversed);
+
+    let student = Student {
+        name: "Walter".into(),
+        id: 582,
+    };
+    let student_ref = StudentRef { student: &student };
+    dbg!(&student);
+    dbg!(student_ref);
+
+    let student_list = StudentList::new(&[("Lyle", 621), ("Anna", 286)]);
+    dbg!(&student_list);
+    dbg!(student_list.find_student_by_id(621));
+    dbg!(student_list.find_student_by_name("Anna"));
+
+    let student_ref_621 = student_list.find_student_by_id(621).unwrap();
+    let student_ref_286 = student_list.find_student_by_id(286).unwrap();
+    dbg!(student_ref_286 == student_ref_621);
+    dbg!(student_ref_286 != student_ref_621);
 }
+
+fn even_or_odd(number: i32) -> &'static str {
+    match number {
+        n if  n % 2 == 0 => "Even",
+        _ => "Odd"
+    }
+}
+
+#[derive(Debug)]
+struct Student {
+    name: String,
+    id: u32,
+}
+
+impl Student {
+    fn new(name: String, id: u32) -> Self {
+        Self { name, id }
+    }
+    fn name(&self) -> &str {
+        self.name.as_ref()
+    }
+    fn id(&self) -> u32 {
+        self.id
+    }
+}
+
+#[derive(Debug)]
+pub struct StudentList {
+    students: Vec<Student>,
+}
+
+#[derive(Debug)]
+pub struct StudentRef<'a> {
+    student: &'a Student,
+}
+
+impl<'a> StudentRef<'a> {
+    fn new(student: &'a Student) -> Self {
+        Self { student }
+    }
+}
+
+impl<'a> Student {
+    fn to_ref(&'a self) -> StudentRef<'a> {
+        StudentRef::new(self)
+    }
+}
+
+impl<'a> PartialEq for StudentRef<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.student.id() == other.student.id()
+    }
+}
+
+impl StudentList {
+    pub fn new(students: &[(&str, u32)]) -> Self {
+    Self {
+        students: students
+            .iter()
+            .map(|(name, id)| {
+                Student::new((*name).into(), *id)
+            })
+            .collect(),
+        }
+    }
+}
+
+impl<'a> StudentList {
+    fn find<F: Fn(&&Student) -> bool>(
+        &'a self,
+        pred: F,
+    ) -> Option<StudentRef<'a>> {
+        self.students.iter()
+            .find(pred)
+            .map(Student::to_ref)
+    }
+    pub fn find_student_by_id(&'a self, id: u32) -> Option<StudentRef<'a>> {
+        self.find(|s| s.id() == id)
+    }
+    pub fn find_student_by_name(
+        &'a self,
+        name: &str,
+    ) -> Option<StudentRef<'a>> {
+        self.find(|s| s.name() == name)
+    }
+}
+
 
 fn fake_bin(s: &str) -> String {
     s
@@ -319,5 +424,35 @@ mod tests {
         assert_eq!(fake_bin("366058562030849490134388085"), "011011110000101010000011011");
         assert_eq!(fake_bin("15889923"), "01111100");
         assert_eq!(fake_bin("800857237867"), "100111001111");
+    }
+
+    fn do_test(number: i32, expected: &str) {
+        let actual = even_or_odd(number);
+        assert_eq!(actual, expected, "\nYour result (left) does not match the expected output (right) for the input {number:?}");
+    }
+
+    #[test]
+    fn test_zero() {
+        do_test(0, "Even");
+    }
+
+    #[test]
+    fn test_positive_even() {
+        do_test(2, "Even");
+    }
+
+    #[test]
+    fn test_positive_odd() {
+        do_test(1, "Odd");
+    }
+
+    #[test]
+    fn test_negative_even() {
+        do_test(-2, "Even");
+    }
+
+    #[test]
+    fn test_negative_odd() {
+        do_test(-1, "Odd");
     }
 }
