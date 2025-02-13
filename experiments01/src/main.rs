fcdc58ff503ba1697d039b279e0ca7d0fa3a98a4
STRATEGIKON-DAN\dan_s <dsabadas@strategikonpharma.com>
2025-02-13 10:29:13 +0200

    another tiny rust

--- experiments01/src/main.rs
+++ experiments01/src/main.rs
@@ -53,6 +53,23 @@ fn main() {
     let count = my_map.values().filter(|&&v| v == 3).count();  
   
     println!("Number of values equal to 3: {}", count);  
+    println!("tribonacci(&[3., 2., 1.], 1): {:?}", tribonacci(&[1., 1., 1.], 1));  
+}
+
+fn tribonacci(signature: &[f64; 3], n: usize) -> Vec<f64> {
+    let mut ret: Vec<f64> = vec![0.; n];
+
+    for i in 0..signature.len() {
+        if i < n {
+            ret[i] = signature[i];
+        }
+    }
+
+    for i in 3..n {
+        ret[i] = ret[i-3] + ret[i-2] + ret[i-1];
+    }
+
+    ret
 }
 
 fn get_count(string: &str) -> usize {
@@ -290,5 +307,17 @@ mod tests {
     #[test]
     fn my_tests_vowels_count() {
         assert_eq!(get_count("abracadabra"), 5);
+    }
+
+    #[test]
+    fn basic_tests_tribonacci1() {
+        assert_eq!(tribonacci(&[0., 1., 1.], 10), vec![0., 1., 1., 2., 4., 7., 13., 24., 44., 81.]);
+        assert_eq!(tribonacci(&[1., 0., 0.], 10), vec![1., 0., 0., 1., 1., 2., 4., 7., 13., 24.]);
+        assert_eq!(tribonacci(&[0., 0., 0.], 10), vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]);
+        assert_eq!(tribonacci(&[1., 2., 3.], 10), vec![1., 2., 3., 6., 11., 20., 37., 68., 125., 230.]);
+        assert_eq!(tribonacci(&[3., 2., 1.], 10), vec![3., 2., 1., 6., 9., 16., 31., 56., 103., 190.]);
+        assert_eq!(tribonacci(&[1., 1., 1.], 1), vec![1.]);
+        assert_eq!(tribonacci(&[300., 200., 100.], 0), vec![]);
+        assert_eq!(tribonacci(&[0.5, 0.5, 0.5], 30), vec![0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5]);
     }
 }
